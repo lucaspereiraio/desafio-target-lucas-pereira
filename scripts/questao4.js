2ea099422e8a0518f96def497a900ece52890367
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-09-02 17:37:23 -0300

    questao4-done

---
+++ scripts/questao4.js
@@ -0,0 +1,8 @@
+function completarSequencia() {
+  document.getElementById("seqA").textContent = "9";
+  document.getElementById("seqB").textContent = "128";
+  document.getElementById("seqC").textContent = "49";
+  document.getElementById("seqD").textContent = "100";
+  document.getElementById("seqE").textContent = "13";
+  document.getElementById("seqF").textContent = "20";
+}
